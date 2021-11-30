9efbc216b39ba7b891e9a4db5732218fb89ce2b9
madgelackie <marionmlackie@gmail.com>
2021-11-30 16:48:31 +0000

    starter structure

---
+++ src/main/java/com/codeclan/filefolderservice/filesHW/FilesHwApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.filefolderservice.filesHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/filefolderservice/filesHW/FilesHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.filefolderservice.filesHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
